eef8844e92fb0e8708e0845e9a9f85bfd15c54da
Nirzari gupta <nirzu97@gmail.com>
2021-06-25 21:43:32 +0530

    Updates in executor and component

--- component/executor.py
+++ component/executor.py
@@ -28,69 +28,76 @@ from tfx.utils import io_utils
 from tfx.components.util import udf_utils
 from tfx.types import standard_component_specs
 
-_DEFAULT_FILE_NAME = 'schema.pbtxt'
+_DEFAULT_FILE_NAME = "schema.pbtxt"
+
 
 class Executor(base_executor.BaseExecutor):
-  """Executor for TFX Schema Curation Custom Component.
+    """Executor for TFX Schema Curation Custom Component.
 
-  This Executor will execute the schema_fn with correct parameters by resolving the input
-  artifacts, output artifacts and execution properties.
-  """
-  
-
-  
-  def Do(self, input_dict: Dict[Text, List[types.Artifact]],
-         output_dict: Dict[Text, List[types.Artifact]],
-         exec_properties: Dict[Text, Any]) -> None:
-    """Uses a user-supplied schema_fn to curate a schema.
-
-    The schema curation Executor invokes a schema_fn callback function provided by
-    the user via the module_file parameter. In this function, user changes the schema.
-
-    Args:
-      input_dict: Input dict from input key to a list of ML-Metadata Artifacts.including:
-        - schema: Schema of the data.A list of type `standard_artifacts.Schema` which should
-          contain a single schema artifact.
-      output_dict: Output dict from output key to a list of Artifacts.
-        - schema: Schema of the data.A list of type `standard_artifacts.Schema` which should
-          contain a single schema artifact.
-      exec_properties: A dict of execution properties, including:
-        - module_file: The file path to a python module file, from which the
-          'schema_fn' function will be loaded. Exactly one of
-          'module_file', 'module_path' and 'schema_fn' should be set.
-        - module_path: The python module path, from which the
-          'schema_fn' function will be loaded. Exactly one of
-          'module_file', 'module_path' and 'schema_fn' should be set.
-        - schema_fn: The module path to a python function that
-          implements 'schema_fn'. Exactly one of 'module_file',
-          'module_path' and 'schema_fn' should be set.
-        
-    Returns:
-      None
-
-    Raises:
-      ValueError: When not exactly one of `module_file`, `module_path` and
-        `schema_fn` are present in 'exec_properties'.
-      RuntimeError: If schema_fn failed to generate modified output schema.
+    This Executor will execute the schema_fn with correct parameters by resolving the input
+    artifacts, output artifacts and execution properties.
     """
-    self._log_startup(input_dict, output_dict, exec_properties)
 
-    if input_dict.get(standard_component_specs.SCHEMA_KEY):
-      schema_file = io_utils.get_only_uri_in_dir(
-        artifact_utils.get_single_uri(
-            input_dict[standard_component_specs.SCHEMA_KEY]))
-    else:
-      schema_file = None
+    def Do(
+        self,
+        input_dict: Dict[Text, List[types.Artifact]],
+        output_dict: Dict[Text, List[types.Artifact]],
+        exec_properties: Dict[Text, Any],
+    ) -> None:
+        """Uses a user-supplied schema_fn to curate a schema.
 
-    schema = io_utils.SchemaReader().read(schema_file)
-    schema_fn = udf_utils.get_fn(exec_properties, 'schema_fn')
-    output_schema = schema_fn(schema)
+        The schema curation Executor invokes a schema_fn callback function provided by
+        the user via the module_file parameter. In this function, user changes the schema.
 
-    output_uri = os.path.join(
-        artifact_utils.get_single_uri(
-            output_dict[standard_component_specs.SCHEMA_KEY]),
-        _DEFAULT_FILE_NAME)
-    io_utils.write_pbtxt_file(output_uri, output_schema)
+        Args:
+          input_dict: Input dict from input key to a list of ML-Metadata Artifacts.including:
+            - schema: Schema of the data.A list of type `standard_artifacts.Schema` which should
+              contain a single schema artifact.
+          output_dict: Output dict from output key to a list of Artifacts.
+            - schema: Schema of the data.A list of type `standard_artifacts.Schema` which should
+              contain a single schema artifact.
+          exec_properties: A dict of execution properties, including:
+            - module_file: The file path to a python module file, from which the
+              'schema_fn' function will be loaded. Exactly one of
+              'module_file', 'module_path' and 'schema_fn' should be set.
+            - module_path: The python module path, from which the
+              'schema_fn' function will be loaded. Exactly one of
+              'module_file', 'module_path' and 'schema_fn' should be set.
+            - schema_fn: The module path to a python function that
+              implements 'schema_fn'. Exactly one of 'module_file',
+              'module_path' and 'schema_fn' should be set.
 
-    logging.info('Schema written to %s.', output_uri)
-    
+        Returns:
+          None
+
+        Raises:
+          ValueError: When not exactly one of `module_file`, `module_path` and
+            `schema_fn` are present in 'exec_properties'.
+        """
+        self._log_startup(input_dict, output_dict, exec_properties)
+
+        if input_dict.get(standard_component_specs.SCHEMA_KEY):
+            schema_file = io_utils.get_only_uri_in_dir(
+                artifact_utils.get_single_uri(
+                    input_dict[standard_component_specs.SCHEMA_KEY]
+                )
+            )
+        else:
+            schema_file = None
+
+        schema = io_utils.SchemaReader().read(schema_file)
+
+        # load and run user defined schema_fn from module file given by user
+        schema_fn = udf_utils.get_fn(exec_properties, "schema_fn")
+        output_schema = schema_fn(schema)
+
+        # Write modified schema back into the pipeline
+        output_uri = os.path.join(
+            artifact_utils.get_single_uri(
+                output_dict[standard_component_specs.SCHEMA_KEY]
+            ),
+            _DEFAULT_FILE_NAME
+        )
+        io_utils.write_pbtxt_file(output_uri, output_schema)
+
+        logging.info("Schema written to %s.", output_uri)
